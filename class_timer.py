8205de60fcb65a47dceaf584323caf71474e3c9f
LV23\konom <konomi@acm.org>
2023-12-03 20:14:58 +0900

    cat

--- class_timer.py
+++ class_timer.py
@@ -20,8 +20,10 @@ min = st.slider('select time (minutes)', max_value=60)
 
 ph = st.empty()
 N = min * 60
-for secs in range(N,0,-1):
+for secs in range(N,-1,-1):
     mm, ss = secs//60, secs%60
     ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
     time.sleep(1)
+    if N > 0 and secs == 0:
+        st.image('happy.jpg', caption='countdown finished')
 
